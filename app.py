f28b5a5791fcd1b4cdc2ed5a7954fd8f6de8fc87
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-08-11 14:19:37 +0200

    app.py -> Datebase Models -> index (/) list all recipes - /route for mor deteils from recipe

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
+    portions = db.Column(db.Integer, nullable=False)
+
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+
+if not os.path.exists('instance/recipes.db'):
+    with app.app_context():
+        db.create_all()
+        create_default_user()
+        print("Datenbank erstellt.")
+
+
+@app.route('/')
+def index():
+    recipes = Recipe.query.all()
+    return render_template('index.html', recipes=recipes)
+
+
+@app.route('/recipe/<int:recipe_id>')
+def recipe_detail(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+    return render_template('recipe_detail.html', recipe=recipe)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
